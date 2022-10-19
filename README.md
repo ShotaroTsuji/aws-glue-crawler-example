80a2c514f5e8e2492e24c7ca04619a30007a32e6
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2022-10-19 18:27:57 +0900

    refactor: datasets are generated by the code

--- README.md
+++ README.md
@@ -1,66 +1,98 @@
-# AWS Glue Crawler の Example
+# An example of AWS Glue Crawler
 
-AWS Glue Crawlerが作成するテーブルを観察するためのexampleです。
-*./glue_crawler_example/data/*および*./glue_crawler_example/non-hive/*に置かれたデータをS3バケットにアップロードし、それぞれに対するクローラを作成します。
+This repository was made to investigate the behaviour of AWS Glue Crawler.
+AWS Glue Crawler is not well documented and it has mysterious behaviour.
 
-Glue Crawlerの実行はAWS Consoleから行ってください。
+Although for a simple usecase,
+such that all of the JSON files in an S3 bucket have the same structure,
+Glue Crawler produces only one table schema,
+when the JSON files don't have the same structure,
+it will produce more than one table schemas.
 
-以下、`cdk init`が作成したREADME.
+## Datasets
 
-## Welcome to your CDK Python project!
+This repository has five datasets to be given to AWS Glue Crawler:
 
-This is a blank project for CDK development with Python.
+- json-data-example
+- flat-and-one-common-key
+- disjoint-keys
+- non-hive-disjoint-keys
+- overlapping-keys
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+These datasets are defined in *glue_crawler_example/data.py*.
 
-This project is set up like a standard Python project.  The initialization
-process also creates a virtualenv within this project, stored under the `.venv`
-directory.  To create the virtualenv it assumes that there is a `python3`
-(or `python` for Windows) executable in your path with access to the `venv`
-package. If for any reason the automatic creation of the virtualenv fails,
-you can create the virtualenv manually.
+The dataset `json-data-example` have only one JSON file.
+Glue Crawler generates only one table schema.
 
-To manually create a virtualenv on MacOS and Linux:
+The dataset `flat-and-one-common-key` have JSON files placed in the root.
+The JSON files have the common key `id`
+but each file has one additional key unique for the file.
+Glue Crawler generates tables for each JSON file.
+
+The dataset `disjoint-keys` have JSON files with a key unique for each file.
+The prefix of the files are in the Hive format.
+Glue Crawler generates many tables.
+
+The dataset `non-hive-disjoint-keys` is a version of the dataset `disjoint-keys`,
+where the prefix isn't in the Hive format.
+Glue Crawler generates many tables.
+
+The dataset `overlapping-keys` have JSON files with the following keys:
+
+- `id`
+- `description`
+- `q0` or `q1`
+
+Glue Crawler generates only one table for this dataset.
+
+## How to deploy
+
+First, you create a virtualenv of Python:
 
 ```console
 $ python3 -m venv .venv
 ```
 
-After the init process completes and the virtualenv is created, you can use the following
-step to activate your virtualenv.
+Second, you activate your virtualenv after the initialization:
 
 ```console
 $ source .venv/bin/activate
 ```
 
-If you are a Windows platform, you would activate the virtualenv like this:
-
-```console
-% .venv\Scripts\activate.bat
-```
-
-Once the virtualenv is activated, you can install the required dependencies.
+Third, you install the dependencies:
 
 ```console
 $ pip install -r requirements.txt
 ```
 
-At this point you can now synthesize the CloudFormation template for this code.
+Then you can deploy the stack:
 
 ```console
-$ cdk synth
+$ cdk deploy
 ```
 
-To add additional dependencies, for example other CDK libraries, just add
-them to your `setup.py` file and rerun the `pip install -r requirements.txt`
-command.
+## How to run the crawlers
 
-## Useful commands
+After the deployment, you can list the crawlers by the following command:
 
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
+```console
+$ inv list-crawlers
+```
 
-Enjoy!
+Then you run the crawlers and wait for their completion:
+
+```console
+$ inv start-crawlers
+```
+
+The command below show the generated table schemas:
+
+```console
+$ inv show-tables
+```
+
+You would like to clean up the databases on AWS Glue:
+
+```console
+$ inv delete-databases
+```
--- glue_crawler_example/glue_crawler_example_stack.py
+++ glue_crawler_example/glue_crawler_example_stack.py
@@ -14,22 +14,53 @@ from . import data
 
 class GlueCrawlerExperiment(Construct):
 
-    def __init__(self, scope: Construct, construct_id: str, prefix: str, bucket: s3.IBucket,
-                 **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str,
+            bucket: s3.IBucket, role: iam.Role, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        if prefix == 'json-data-example':
-            sources = data.json_data_example()
-        elif prefix == 'flat-and-one-common-key':
-            sources = data.flat_and_one_common_key()
+        if construct_id == 'JsonDataExample':
+            dataset = data.JsonDataExample()
+        elif construct_id == 'FlatAndOneCommonKey':
+            dataset = data.FlatAndOneCommonKey()
+        elif construct_id == 'DisjointKeys':
+            dataset = data.DisjointKeys()
+        elif construct_id == 'NonHiveDisjointKeys':
+            dataset = data.NonHiveDisjointKeys()
+        elif construct_id == 'OverlappingKeys':
+            dataset = data.OverlappingKeys()
         else:
-            sources = [s3_deploy.Source.asset(os.path.join(os.path.dirname(__file__), prefix))]
+            assert False
 
         s3_deploy.BucketDeployment(self,
                                    "InputJsonBucketDeployment",
                                    destination_bucket=bucket,
-                                   destination_key_prefix=f"{prefix}/",
-                                   sources=sources)
+                                   destination_key_prefix=f"{dataset.prefix}/",
+                                   retain_on_delete=False,
+                                   sources=dataset.sources)
+
+        glue.CfnCrawler(
+            self,
+            "Crawler",
+            role=role.role_name,
+            targets=glue.CfnCrawler.TargetsProperty(s3_targets=[
+                glue.CfnCrawler.S3TargetProperty(
+                    path=f"s3://{bucket.bucket_name}/{dataset.prefix}/", )
+            ], ),
+            database_name=
+            f"glue_crawler_experiment_{'_'.join(dataset.prefix.split('-'))}",
+            tags={"glue-crawler-experiment": dataset.prefix},
+        )
+
+
+class GlueCrawlerExampleStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(self,
+                           "InputJsonBucket",
+                           removal_policy=RemovalPolicy.DESTROY,
+                           auto_delete_objects=True)
 
         role = iam.Role(
             self,
@@ -47,29 +78,8 @@ class GlueCrawlerExperiment(Construct):
                 actions=["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
             ))
 
-        glue.CfnCrawler(
-            self,
-            "Crawler",
-            role=role.role_name,
-            targets=glue.CfnCrawler.TargetsProperty(s3_targets=[
-                glue.CfnCrawler.S3TargetProperty(
-                    path=f"s3://{bucket.bucket_name}/{prefix}/", )
-            ], ),
-            database_name=
-            f"glue_crawler_experiment_{'_'.join(prefix.split('-'))}",
-            tags={"glue-crawler-experiment": prefix},
-        )
-
-
-class GlueCrawlerExampleStack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-
-        bucket = s3.Bucket(self, "InputJsonBucket", removal_policy=RemovalPolicy.DESTROY)
-
-        GlueCrawlerExperiment(self, "DisjointKeys", "disjoint-keys", bucket)
-        GlueCrawlerExperiment(self, "NonHiveDisjointKeys", "non-hive-disjoint-keys", bucket)
-        GlueCrawlerExperiment(self, "OverlappingKeys", "overlapping-keys", bucket)
-        GlueCrawlerExperiment(self, "JsonDataExample", "json-data-example", bucket)
-        GlueCrawlerExperiment(self, "FlatAndOneCommonKey", "flat-and-one-common-key", bucket)
+        GlueCrawlerExperiment(self, "DisjointKeys", bucket, role)
+        GlueCrawlerExperiment(self, "NonHiveDisjointKeys", bucket, role)
+        GlueCrawlerExperiment(self, "OverlappingKeys", bucket, role)
+        GlueCrawlerExperiment(self, "JsonDataExample", bucket, role)
+        GlueCrawlerExperiment(self, "FlatAndOneCommonKey", bucket, role)
